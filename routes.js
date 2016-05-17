e1b94bebdff9492c0d83b7ae190519041b983faa
Albert Duran <albertrdg@gmail.com>
2016-05-17 12:58:47 -0300

    added callback external API gosocket

--- routes.js
+++ routes.js
@@ -1,6 +1,9 @@
 module.exports = function(app) {
 
 	var SerieTV = require('./seriestv');
+	var http = require('http');
+	var urlParser = require('url');
+
 
 	//GET All
 	findAllSeriesTV = function(req, res) {
@@ -58,9 +61,54 @@ module.exports = function(app) {
 		SerieTV.findById(req.params.id, function(err, seriestv){
 			seriestv.remove(function(err){
 				if(!err) console.log('Serie Borrada');
-			else console.log('ERROR: ' + err);
+				else console.log('ERROR: ' + err);
 			});
 		});
+	};
+
+	otro = function(callback) {
+		var uri = urlParser.parse('http://api.sandbox.gosocket.net/api/gadget/getuser');
+		var data ="";
+		var options = {
+			hostname: uri.hostname,
+			port: uri.port,
+			path: uri.path,
+			auth: 'ba9830a4-774d-4f2e-abaa-349a7954f5bb:572f7db6'
+		};
+		console.log(options);
+
+
+		http.get(options, function(res) {
+		  console.log("Got response: " + res.statusCode);
+		  console.log(res);
+		  res.on("data", function(chunk) {
+		    console.log("BODY: " + chunk);
+		    console.log(typeof chunk),
+		    data = chunk;
+		    callback(null, data.toString());
+		  });
+		  
+		}).on('error', function(e) {
+		  console.log("Got error: " + e.message);
+		  callback(e.message, null)
+		});
+
+
+	};
+
+	//Call
+	getVendors = function(req, res) {
+		otro(function(err, data) {
+			console.log('IF FFF');
+			if (!err) {
+				res.json({data:data});
+			} 
+			else {
+				res.status(500).json({err:err,data:data});
+				console.log(err);
+			}
+		});
+
 	};
 
 	//API Routes
@@ -69,4 +117,5 @@ module.exports = function(app) {
 	app.post('/seriestv', addSerieTV);
 	app.put('/seriestv/:id',updateSerieTV);
 	app.delete('/seriestv/:id', deleteSerieTV);
+	app.get('/call', getVendors);
 };
